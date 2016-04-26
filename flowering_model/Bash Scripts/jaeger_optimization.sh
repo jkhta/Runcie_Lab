e13ab78fb7fe2d553081dfebba6f297372d7ac43
James Ta <jkhta@campus-055-250.ucdavis.edu>
2016-04-26 15:51:12 -0700

    Create general directory and subdirectories for organization.

---
+++ flowering_model/Bash Scripts/jaeger_optimization.sh
@@ -0,0 +1,10 @@
+#! /bin/bash -l
+#SBATCH -D /home/jkta/projects/jaeger
+#SBATCH -o /home/jkta/projects/jaeger/logs/output/out-%j.txt
+#SBATCH -e /home/jkta/projects/jaeger/logs/error/error-%j.txt
+#SBATCH -J jaeger_opt
+
+module load R
+
+R CMD BATCH --no-save --no-restore "--args ${SLURM_ARRAY_TASK_ID}" optimization_batch.R logs/Rout-${SLURM_ARRAY_TASK_ID}.txt
+echo "Done"
